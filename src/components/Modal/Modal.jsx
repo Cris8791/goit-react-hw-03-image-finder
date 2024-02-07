cebbb1f94cf2c28ef45f50f3e40212c7d3601330
Cris8791 <135622871+Cris8791@users.noreply.github.com>
2024-02-07 20:42:58 +0200

    commit6

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,14 +1,29 @@
+// Modal.jsx
 import React, { Component } from 'react';
 import styles from './Modal.module.css';
+import Loader from '../Loader/Loader'; // Importăm Loader-ul
 
 class Modal extends Component {
+  state = {
+    loading: true, // Adăugăm un state local pentru încărcare
+  };
+
   componentDidMount() {
+    this.preloadImage(this.props.image.largeImageURL);
     window.addEventListener('keydown', this.handleKeyDown);
   }
 
   componentWillUnmount() {
     window.removeEventListener('keydown', this.handleKeyDown);
   }
+
+  preloadImage = url => {
+    const img = new Image();
+    img.src = url;
+    img.onload = () => {
+      this.setState({ loading: false }); // Actualizăm starea când imaginea s-a încărcat
+    };
+  };
 
   handleKeyDown = e => {
     if (e.code === 'Escape') {
@@ -17,10 +32,15 @@ class Modal extends Component {
   };
 
   render() {
+    const { loading } = this.state; // Extragem starea locală
+
     return (
       <div className={styles.overlay} onClick={this.props.onClose}>
         <div className={styles.modal}>
-          <img src={this.props.image.largeImageURL} alt="" />
+          {loading && <Loader />}{' '}
+          {/* Afisăm Loader-ul dacă imaginea se încarcă */}
+          {!loading && <img src={this.props.image.largeImageURL} alt="" />}{' '}
+          {/* Afisăm imaginea dacă s-a încărcat complet */}
         </div>
       </div>
     );
